8e0429a33853cdc0c22554ecd82c69b128ac5afa
artevely <artemyveliky02@gmail.com>
2024-04-25 22:19:06 +0400

    feat: add solution 25.04.2024

---
+++ april/25_04_2370longestIdealSubsequence.py
@@ -0,0 +1,43 @@
+# You are given a string s consisting of lowercase letters and an integer k. We call a string t ideal if the following conditions are satisfied:
+
+#     t is a subsequence of the string s.
+#     The absolute difference in the alphabet order of every two adjacent letters in t is less than or equal to k.
+
+# Return the length of the longest ideal string.
+
+# A subsequence is a string that can be derived from another string by deleting some or no characters without changing the order of the remaining characters.
+
+# Note that the alphabet order is not cyclic. For example, the absolute difference in the alphabet order of 'a' and 'z' is 25, not 1.
+
+
+# Example 1:
+
+# Input: s = "acfgbd", k = 2
+# Output: 4
+# Explanation: The longest ideal string is "acbd". The length of this string is 4, so 4 is returned.
+# Note that "acfgbd" is not ideal because 'c' and 'f' have a difference of 3 in alphabet order.
+
+# Example 2:
+
+# Input: s = "abcd", k = 3
+# Output: 4
+# Explanation: The longest ideal string is "abcd". The length of this string is 4, so 4 is returned.
+
+class Solution:
+    def longestIdealString(self, s: str, k: int) -> int:
+
+        dp = [0] * 26
+
+        for c in s:
+            i = ord(c) - ord('a')
+            dp[i] = 1 + self._getMaxReachable(dp, i, k)
+
+        return max(dp)
+
+    def _getMaxReachable(self, dp: List[int], i: int, k: int) -> int:
+        first = max(0, i - k)
+        last = min(25, i + k)
+        maxReachable = 0
+        for j in range(first, last + 1):
+            maxReachable = max(maxReachable, dp[j])
+        return maxReachable
