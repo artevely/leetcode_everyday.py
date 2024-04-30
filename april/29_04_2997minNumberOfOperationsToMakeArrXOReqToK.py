42501d37b491b1f56577479ad027d7b99bc90c25
artevely <artemyveliky02@gmail.com>
2024-04-30 09:50:21 +0400

    feat: add solution 30.04.2024

--- april/29_04_2997minNumberOfOperationsToMakeArrXOReqToK.py
+++ april/29_04_2997minNumberOfOperationsToMakeArrXOReqToK.py
@@ -27,10 +27,10 @@
 
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
-        x = 0
+        x = 0 
         for c in nums:
             x ^= c
-
+        
         count = 0
         for i in range(32):
             if ((x & (1 << i)) > 0) != ((k & (1 << i)) > 0):
